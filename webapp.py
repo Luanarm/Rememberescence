70a837d18beef7b141396ac2777e036d1390d0eb
Yihoi Jung <yihoi0311@gmail.com>
2024-01-27 00:39:52 -0500

    Basic Cohere Implementation
    HTML file + Virtual Env + Flask Python file

--- webapp.py
+++ webapp.py
@@ -1,14 +1,38 @@
-from flask import Flask, make_response, redirect,render_template,request, send_from_directory
+from flask import Flask, render_template, request, jsonify
+import cohere
+import os
 
 app = Flask(__name__)
-config = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
 
-# tell Flask to use the above defined config
-app.config.from_mapping(config)
+#cohere_api_key = os.getenv("CO_API_KEY")
+cohere_api_key = "s5yafntwVwUN7J108V11n9y4YceYj9ZvjAzBMAjI"
+print(f"API Key: {cohere_api_key}")
+co = cohere.Client(cohere_api_key)
+
 @app.route("/")
-def main():
+def home():
     return render_template("home.html")
+
+@app.route("/generate", methods=["POST"])
+def generate_content():
+    data = request.json
+
+    # Get user inputs from the JSON data
+    user_interests = data.get("interests")
+    user_location = data.get("location")
+    #user_interests = "legos, beyblade, star wars"
+    #user_location = "Canada"
+
+    # Use Cohere to generate content based on user inputs
+    generation_response = co.generate(
+        prompt=f'Please write a short story for nostalgia based on these parameters: {user_interests} {user_location}'
+    )
+
+    # Access the 'text' attribute to get the generated content
+    generated_content = generation_response[0].text
+
+    # Return the JSON response with the generated content
+    return jsonify({"generated_content": generated_content})
+
+if __name__ == '__main__':
+    app.run(debug=True)
