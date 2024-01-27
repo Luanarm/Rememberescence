1cd4c49c25ffd5f9b02959de6e5090966ab8e8c6
Yihoi Jung <yihoi0311@gmail.com>
2024-01-27 13:46:58 -0500

    OAuth Authentication implemented

--- webapp.py
+++ webapp.py
@@ -3,6 +3,13 @@ import cohere
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
 
 app = Flask(__name__)
 
@@ -16,9 +23,59 @@ client_credentials_manager = SpotifyClientCredentials(client_id='55be60be52eb4b6
                                                       client_secret='8ce1ad629229430ab7d2d7278ebdb9bd')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    if session.get('user'):
+        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    else:
+        return render_template("login.html")
 
 @app.route("/generate", methods=["POST"])
 def generate_content():
@@ -35,7 +92,7 @@ def generate_content():
     # Use Cohere to generate content based on user inputs
     parameters = f'Friends:{user_friends}, Interests: {user_interests}, Location: {user_location}'
     generation_response = co.generate(
-        prompt=f'Please write a short story for nostalgia based on these parameters: {parameters} addressing the user as "you"'
+        prompt=f'Please write a nostalgic short story about the users childhood based on these parameters: {parameters} addressing the user as "you"'
     )
 
     # User's age & location used for childhood Spotify
