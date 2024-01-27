8c8c367a6152aa7bfa3f23873cf8b98b5e5ed06c
Yihoi Jung <yihoi0311@gmail.com>
2024-01-27 11:35:07 -0500

    Fixed text generation for cohere portion

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import cohere
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
@@ -8,6 +10,11 @@ app = Flask(__name__)
 cohere_api_key = "s5yafntwVwUN7J108V11n9y4YceYj9ZvjAzBMAjI"
 print(f"API Key: {cohere_api_key}")
 co = cohere.Client(cohere_api_key)
+
+# Initialize Spotipy client
+client_credentials_manager = SpotifyClientCredentials(client_id='55be60be52eb4b658763757653391641', 
+                                                      client_secret='8ce1ad629229430ab7d2d7278ebdb9bd')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.route("/")
 def home():
@@ -20,17 +27,19 @@ def generate_content():
     # Get user inputs from the JSON data
     user_interests = data.get("interests")
     user_location = data.get("location")
-    user_age = data.get("location")
-    user_friends = data.get("location")
+    user_age = data.get("age")
+    user_friends = data.get("friends")
+
+    print(user_friends)
 
     # Use Cohere to generate content based on user inputs
     parameters = f'Friends:{user_friends}, Interests: {user_interests}, Location: {user_location}'
     generation_response = co.generate(
-        prompt=f'Please write a short story for nostalgia based on these parameters: {parameters}'
+        prompt=f'Please write a short story for nostalgia based on these parameters: {parameters} addressing the user as "you"'
     )
 
     # User's age & location used for childhood Spotify
-    
+
 
     # Access the 'text' attribute to get the generated content
     generated_content = generation_response[0].text
